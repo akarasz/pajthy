3216838b8b65d19659dcbd350afb7abb7aecf095
Andras Karasz <andras.karasz@logmein.com>
2020-12-20 22:18:12 +0100

    feat: skeletons from sitebuild

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,55 @@
-import './App.css';
+import { useState } from "react"
+import { BrowserRouter as Router, Switch, Route, useParams } from "react-router-dom"
+import "./App.css"
 
 const App = () => (
   <div className="App">
     <Header />
     <Countdown />
-    <Control />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home title="Alpha Team Grooming 1st of April" />
+        </Route>
+        <Route path="/:sessionId/control">
+          <Control title="Alpha Team Grooming 1st of April" />
+        </Route>
+        <Route path="/:sessionId">
+          <Session title="Alpha Team Grooming 1st of April" />
+        </Route>
+      </Switch>
+    </Router>
   </div>)
 
 const Header = () => (
   <header>
-    <logo></logo>
+    <div className="logo"></div>
     <h1>Pajthy</h1>
   </header>)
 
 const Countdown = () => (
-  <countdown></countdown>)
+  <div className="countdown"></div>)
 
-const Landing = () => (
-  <content>
-    <TextInput name="Session" placeholder="Alpha Team Grooming 1st of April" />
+const Home = ({ title }) => (
+  <div class="content">
+    <TextInput name="Session" placeholder={title} />
     <BigButton text="Fibonacci" />
     <BigButton text="T-Shirt" />
-  </content>)
+  </div>)
 
-const Control = () => (
-  <content class="admin">
-    <h2>Alpha Team Grooming 1st of April</h2>
-    <div class="share">
-      <div class="link">https://pajthy.com/abcde</div>
-      <div class="button">Copy</div>
+const Control = ({ title }) => {
+  let { sessionId } = useParams()
+
+  return (<div className="admin content">
+    <h2>{title}</h2>
+    <div className="share">
+      <div className="link">https://pajthy.com/{sessionId}</div>
+      <div className="button">Copy</div>
     </div>
     <hr />
     <BigButton text="Begin voting!" />
     <hr />
-    <table class="votes">
+    <table className="votes">
       <tbody>
         <tr>
           <td>Alice (x)</td>
@@ -49,15 +65,52 @@ const Control = () => (
         </tr>
       </tbody>
     </table>
-  </content>)
+  </div>)
+}
 
-const TextInput = ({ name, placeholder }) => (
-  <div class="pair">
-    <label for={name}>{name}</label>
-    <input id={name} type="text" placeholder={placeholder} />
+const Session = ({ title }) => {
+  const [name, setName] = useState(null)
+
+  if (name === null) {
+    return <JoinForm title={title} setName={setName} />
+  } else {
+    return <Vote title={title} />
+  }
+}
+
+const JoinForm = ({ title, setName }) => {
+  const [value, setValue] = useState(null)
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  return (<div className="content">
+    <h2>{title}</h2>
+    <TextInput name="Name" onChange={handleChange} />
+    <BigButton text="Join" onClick={() => setName(value)} />
+  </div>)
+}
+
+const Vote = ({ title }) => (
+  <div className="content">
+    <h2>{title}</h2>
+    <BigButton text="1" />
+    <BigButton text="2" />
+    <BigButton text="3" />
+    <BigButton text="5" />
+    <BigButton text="8" />
+    <BigButton text="13" />
+    <BigButton text="?" />
   </div>)
 
-const BigButton = ( { text }) => (
-  <button class="big">{text}</button>)
+const TextInput = ({ name, placeholder, onChange }) => (
+  <div className="pair">
+    <label htmlFor={name}>{name}</label>
+    <input id={name} type="text" placeholder={placeholder} onChange={onChange} />
+  </div>)
 
-export default App;
+const BigButton = ({ text, onClick }) => (
+  <button onClick={onClick} className="big">{text}</button>)
+
+export default App
